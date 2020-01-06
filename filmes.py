f58ae4fbc6ef915fffdb1e7757622c1ed35cab17
Ronald Bolsoni Falcão <ronald@ronaldfalcao.com.br>
2020-01-05 21:37:38 -0300

    Encapsulando os métodos criados para a classe Filme.

--- filmes.py
+++ filmes.py
@@ -1,10 +1,43 @@
 class Filme:
 
     def __init__(self, nome, ano, duracao, likes):
-        self.nome = nome.title()
+        self.__nome = nome.title()
+        self.__ano = ano
+        self.__duracao = duracao
+        self.__likes = likes
+
+    """ Métodos de exibição dos atributos"""
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @property
+    def duracao(self):
+        return self.__duracao
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @ano.setter
+    def ano(self, ano):
         self.ano = ano
-        self.duracao = duracao
-        self.likes = likes
+
+    @duracao.setter
+    def duracao(self, duracao):
+        self.__duracao = duracao
+
+    @likes.setter
+    def likes(self, likes):
+        self.__likes = likes
 
     def dar_likes(self):
         self.likes += 1
@@ -13,13 +46,15 @@ class Filme:
 class Serie:
 
     def __init__(self, nome, ano, temporadas, likes):
-        self.nome = nome.title()
-        self.ano = ano
-        self.temporadas = temporadas
-        self.likes = likes
+        self.__nome = nome.title()
+        self.__ano = ano
+        self.__temporadas = temporadas
+        self.__likes = likes
 
     def dar_likes(self):
-        self.likes += 1
+        self.__likes += 1
 
 
 filme = Filme("a morte do caixeiro viajante", 1951, 105, 0)
+filme.dar_likes()
+print(filme.likes)
