fc5d988c0994effdb7d468fbb340c262275aa441
Ronald Bolsoni Falcão <ronald@ronaldfalcao.com.br>
2020-02-17 19:18:47 -0300

    Voltando ao curso de Pyhton Manipulação de Strings para finalizar o código.

--- manipulacao_strings/extrator.py
+++ manipulacao_strings/extrator.py
@@ -1,4 +1,4 @@
-class ExtratorArgumentosUrl():
+class ExtratorArgumentosUrl:
 
     def __init__(self, url):
         if self.url_eh_valida(url):
@@ -12,4 +12,24 @@ class ExtratorArgumentosUrl():
             return True
         else:
             return False
+
+    def encontra_indice_inicio_substring(self, moeda_ou_valor):
+        return self.url.find(moeda_ou_valor) + len(moeda_ou_valor) + 1
+
+    def retorna_moedas(self):
+
+        busca_moeda_origem = "moedaorigem"
+        busca_moeda_destino = "moedadestino"
+
+        inicio_substring_moeda_origem = self.encontra_indice_inicio_substring(busca_moeda_origem)
+        final_substring_moeda_origem = self.url.find("&")
+        moeda_origem = self.url[inicio_substring_moeda_origem:final_substring_moeda_origem]
+
+        inicio_substring_moeda_destino = self.encontra_indice_inicio_substring(busca_moeda_destino)
+
+        final_substring_moeda_destino = self.url.find("&valor")
+        moeda_destino = self.url[inicio_substring_moeda_destino:]
+
+        return moeda_origem, moeda_destino
+
 
