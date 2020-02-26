3cb2ea005f13f60f1248f7d8b998f1c0b5c21bce
Ronald Bolsoni Falcão <ronald@ronaldfalcao.com.br>
2020-02-26 12:39:52 -0300

    Alterando o nome da classe e do arquivo por conta da refatoração. Agora a classe Avaliador() não existe mais.

--- leilao/src/leilao/test_dominio.py
+++ leilao/src/leilao/test_dominio.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from leilao.src.leilao.dominio import Usuario, Lance, Leilao
 
 
-class TestAvaliador(TestCase):
+class TestLeilao(TestCase):
 
     def setUp(self):
         """
