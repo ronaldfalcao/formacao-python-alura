3bf3ac0c1ea0b86251aad3d8bcd5c6781b22ba70
Ronald Bolsoni Falcão <ronald@ronaldfalcao.com.br>
2020-02-26 20:53:22 -0300

    Criando método criar_usuario utilizando o Pytest em substituição da repetiçao do código nos métodos de teste.

--- leilao/tests/test_usuario.py
+++ leilao/tests/test_usuario.py
@@ -2,37 +2,38 @@ from leilao.src.leilao.dominio import Usuario, Leilao
 import pytest
 
 
-def test_descontar_valor_lance_carteira_usuario():
+@pytest.fixture
+def criar_usuario():
+    return Usuario("A", 100)
 
-    usuario_a = Usuario("A", 100)
+
+def test_descontar_valor_lance_carteira_usuario(criar_usuario):
+
     leilao = Leilao("Exemplo Leilão")
-    usuario_a.propor_lance(leilao, 50)
+    criar_usuario.propor_lance(leilao, 50)
 
-    assert usuario_a.carteira == 50
+    assert criar_usuario.carteira == 50
 
 
-def test_permite_lance_menor_total_carteira_usuario():
+def test_permite_lance_menor_total_carteira_usuario(criar_usuario):
 
-    usuario_a = Usuario("A", 100)
     leilao = Leilao("Exemplo Leilão")
-    usuario_a.propor_lance(leilao, 1)
+    criar_usuario.propor_lance(leilao, 1)
 
-    assert usuario_a.carteira == 99.0
+    assert criar_usuario.carteira == 99.0
 
 
-def test_permite_lance_valor_igual_valor_carteira_usuario():
+def test_permite_lance_valor_igual_valor_carteira_usuario(criar_usuario):
 
-    usuario_a = Usuario("A", 100)
     leilao = Leilao("Exemplo Leilão")
-    usuario_a.propor_lance(leilao, 100)
+    criar_usuario.propor_lance(leilao, 100)
 
-    assert usuario_a.carteira == 0
+    assert criar_usuario.carteira == 0
 
 
-def test_nao_permite_lance_acima_valor_carteira_usuario():
+def test_nao_permite_lance_acima_valor_carteira_usuario(criar_usuario):
 
     with pytest.raises(ValueError):
-        usuario_a = Usuario("A", 100)
         leilao = Leilao("Exemplo Leilão")
-        usuario_a.propor_lance(leilao, 200)
+        criar_usuario.propor_lance(leilao, 200)
 
