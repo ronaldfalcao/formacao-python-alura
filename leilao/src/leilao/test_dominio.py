41a2f996603a1bc068598192f52e345a9c24b3f8
Ronald Bolsoni Falcão <ronald@ronaldfalcao.com.br>
2020-02-26 09:55:51 -0300

    Criação dos vários métodos de teste para testar quantidade de lances possíveis.

--- leilao/src/leilao/test_dominio.py
+++ leilao/src/leilao/test_dominio.py
@@ -4,7 +4,10 @@ from leilao.src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 class TestAvaliador(TestCase):
 
-    def test_avaliar(self):
+    def test_avaliar_quando_dois_lances(self):
+        """
+        Teste para avaliar o cenário com dois lances.
+        """
 
         usuario_a = Usuario("A")
         usuario_b = Usuario("B")
@@ -23,7 +26,11 @@ class TestAvaliador(TestCase):
         self.assertEqual(100.00, avaliador.menor_lance)
         self.assertEqual(110.00, avaliador.maior_lance)
 
-    def test_avaliar_ordem_invertida(self):
+    def test_avaliar_quando_ordem_invertida(self):
+        """
+        Teste para avaliar da mesma forma que o teste acima (test_avaliar), mas invertendo
+        a ordem de inserção dos lances com o critério de maior e menor valores invertidos.
+        """
 
         usuario_a = Usuario("A")
         usuario_b = Usuario("B")
@@ -41,3 +48,48 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(100.00, avaliador.menor_lance)
         self.assertEqual(110.00, avaliador.maior_lance)
+
+    def test_avaliar_quando_houver_um_lance(self):
+        """
+        Teste para verificar as regras se houver apenas um lance.
+        """
+
+        usuario_a = Usuario("A")
+
+        lance_usuario_a = Lance(usuario_a, 100.0)
+
+        leilao = Leilao("Leilão de Exemplo")
+
+        leilao.lances.append(lance_usuario_a)  # Inverteu essas linhas
+
+        avaliador = Avaliador()
+        avaliador.avaliar(leilao)
+
+        self.assertEqual(100.00, avaliador.menor_lance)
+        self.assertEqual(100.00, avaliador.maior_lance)
+
+    def test_avaliar_quando_tres_lances(self):
+        """
+        Teste para avaliar da mesma forma que o teste acima (test_avaliar), mas invertendo
+        a ordem de inserção dos lances com o critério de maior e menor valores invertidos.
+        """
+
+        usuario_a = Usuario("A")
+        usuario_b = Usuario("B")
+        usuario_c = Usuario("C")
+
+        lance_usuario_a = Lance(usuario_a, 100.0)
+        lance_usuario_b = Lance(usuario_b, 110.0)
+        lance_usuario_c = Lance(usuario_c, 200.0)
+
+        leilao = Leilao("Leilão de Exemplo")
+
+        leilao.lances.append(lance_usuario_a)  # Inverteu essas linhas
+        leilao.lances.append(lance_usuario_b)
+        leilao.lances.append(lance_usuario_c)
+
+        avaliador = Avaliador()
+        avaliador.avaliar(leilao)
+
+        self.assertEqual(100.00, avaliador.menor_lance)
+        self.assertEqual(200.00, avaliador.maior_lance)
