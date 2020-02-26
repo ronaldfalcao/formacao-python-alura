b19a2c6d3a977030d1e4e7354ec2f5005915084b
Ronald Bolsoni Falcão <ronald@ronaldfalcao.com.br>
2020-02-26 13:23:08 -0300

    Melhoria (pythônica) na comparação da lista vazia, retirando o len() e a comparação com "== 0".

--- leilao/src/leilao/dominio.py
+++ leilao/src/leilao/dominio.py
@@ -49,7 +49,7 @@ class Leilao:
 
         """
 
-        if len(self.lances) == 0 or self.__lances[-1].usuario != lance.usuario:
+        if not self.lances or self.__lances[-1].usuario != lance.usuario:
             if lance.valor > self.maior_lance:
                 self.maior_lance = lance.valor
             if lance.valor < self.menor_lance:
